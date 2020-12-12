96588ad040d5f149c6a5ae0e0ac27507a100c940
Alex <alex.herreid@gmail.com>
2020-12-11 21:38:55 -0500

    Day 1, Challenge 2

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -57,7 +57,7 @@ namespace AdventOfCode2020
 										Day1_Puzzle1();
 										break;
 									case DayPuzzleType.Puzzle2:
-
+										Day1_Puzzle2();
 										break;
 								}
 								break;
@@ -125,7 +125,7 @@ namespace AdventOfCode2020
 		static void Day1_Puzzle1()
 		{
 			Console.Clear();
-			Console.WriteLine("Day 1 Challenge Summary: Elves accounting expense report.");
+			Console.WriteLine("Day 1 Challenge 1 Summary: Elves accounting expense report.");
 			Console.WriteLine("Given a list of int's find the two values that add up to 2020 and then multiple them by each other.");
 			Console.WriteLine("***********************");
 			Console.WriteLine("Input(s): List of int (\"expenses\") of length N");
@@ -192,6 +192,93 @@ namespace AdventOfCode2020
 						{
 							Console.WriteLine($"The multiplication of one option comes out to: {s2020.Item1 * s2020.Item2}");
 						}
+					}
+				}
+			}
+
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadLine();
+		}
+
+		static void Day1_Puzzle2()
+		{
+			Console.Clear();
+			Console.WriteLine("Day 1 Challenge 2 Summary: Elves accounting expense report.");
+			Console.WriteLine("Given a list of int's find the three values that add up to 2020 and then multiple them by one another.");
+			Console.WriteLine("***********************");
+			Console.WriteLine("Input(s): List of int (\"expenses\") of length N");
+			Console.WriteLine("***********************");
+			Console.WriteLine();
+
+			Console.WriteLine("Paste (or type) a list of int's with one per line. Any line that has other characters will be discarded. Processing stops when an empty line is detected.");
+			Console.WriteLine("Expenses:");
+
+			List<int> rawExpenses = new List<int>();
+			string line;
+			while ((line = Console.ReadLine()) != null)
+			{
+				if (string.IsNullOrEmpty(line))
+				{
+					break;
+				}
+				else
+				{
+					if (int.TryParse(line.Trim(), out int exp))
+					{
+						rawExpenses.Add(exp);
+					}
+				}
+			}
+
+			Console.WriteLine($"Inputs complete, found {rawExpenses.Count} expenses");
+			if (rawExpenses.Count > 0)
+			{
+				//Find the three on the list that total 2020
+				List<Tuple<int, int, int>> sumTo2020 = new List<Tuple<int, int, int>>();
+				int[] expenses = rawExpenses.ToArray();
+
+				//Loop through each item in the array
+				for (int i = 0; i < expenses.Length; i++)
+				{
+					//and for each item in the array, go through the array to sum them up
+					//skip the case where i == j as that is just summing the same item twice
+					for (int j = 0; j < expenses.Length; j++)
+					{
+						for (int k = 0; k < expenses.Length; k++)
+						{
+							if (expenses[i] + expenses[j] + expenses[k] == 2020)
+							{
+								sumTo2020.Add(new Tuple<int, int, int>(expenses[i], expenses[j], expenses[k]));
+							}
+						}
+					}
+				}
+
+				int possibleSumTo2020 = sumTo2020.Count;
+				Console.WriteLine($"Found {possibleSumTo2020} total possibilities (including dupes)");
+				if (sumTo2020.Count > 0)
+				{
+					List<int> multiplicationResults = new List<int>();
+
+					foreach (Tuple<int, int, int> options in sumTo2020)
+					{
+						multiplicationResults.Add(options.Item1 * options.Item2 * options.Item3);
+					}
+					
+					//de-dupe
+					multiplicationResults = multiplicationResults.Distinct().ToList();
+					
+					if (multiplicationResults.Count > 1)
+					{
+						Console.WriteLine("CAUTION: MULTIPLE MATCHES");
+						foreach (int r in multiplicationResults)
+						{
+							Console.WriteLine($"The multiplication of one option comes out to: {r}");
+						}
+					}
+					else
+					{
+						Console.WriteLine("Result is: " + multiplicationResults.First());
 					}
 				}
 			}
